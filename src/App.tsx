63b41a0134299e4000b5c53c8a13364d6d6e3f94
osh <karol.slomczynski@gmail.com>
2020-11-28 13:35:07 +0000

    xo5

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [turn, setTurn] = useState('x')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {turn}
+      <table cellSpacing={0} cellPadding={0}>
+        <tbody>
+        {new Array(50).fill('').map((_, i) => <tr key={i}>
+          {new Array(50).fill('').map((_, j) => <td key={j}>
+            <input type="button" style={{ border: '1px solid grey', width: '15px', height: '15px' }}
+                   onClick={(e) => {
+                     // @ts-ignore
+                     if (!e.target.value) {
+                       // @ts-ignore
+                       e.target.value = turn
+                       setTurn(turn === 'x' ? 'o' : 'x')
+                     }
+                     return false
+                   }}/>
+          </td>)}
+        </tr>)}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
